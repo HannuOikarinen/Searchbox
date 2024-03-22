c31e82505d16c3d2198cc2604a150c36b39199ec
HannuOikarinen <oikarhan@gmail.com>
2024-03-22 08:37:27 +0200

    lisätty

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React, { useEffect, useState } from 'react';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Search from './components/Search';
+import Row from './components/Row'; // Assuming you have a Row component
+import { DATA } from './DATA';
 
-export default function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    setItems(DATA);
+  }, []);
+
+  const executeSearch = (search) => {
+    const searchArray = DATA.filter((item) => item.lastname.toLowerCase().startsWith(search.toLowerCase()));
+    setItems(searchArray);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Search executeSearch={executeSearch} />
+      <FlatList
+        data={items}
+        renderItem={({ item }) => (
+        <Row person={item} />
+  )}
+  ></FlatList>
+      </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 100,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+export default App;
---
+++ DATA.js
@@ -0,0 +1,12 @@
+export const DATA = [
+    {
+        id: '1',
+        lastname: 'Juntunen',
+        firstname: 'jouni'
+    },
+    {
+        id: '2',
+        lastname: 'Gacy',
+        firstname: 'John'
+    }
+]
